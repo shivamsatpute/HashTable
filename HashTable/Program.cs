887b2a55cf5268c3ef076b0a614018fc92c6383b
shivam satpute <shivam897satpute@gmail.com>
2021-05-31 19:45:40 +0530

    [shivam]add: Creating Hash Table and Adding Item

---
+++ HashTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hashtable = new MyMapNode<string, string>(5); 
+            hashtable.Add("0", "To");  
+            hashtable.Add("1", "be");
+            hashtable.Add("2", "or");
+            hashtable.Add("3", "not");
+            hashtable.Add("4", "to");
+            hashtable.Add("5", "be");
+            Console.ReadLine();
+        }
+    }
+}
+
